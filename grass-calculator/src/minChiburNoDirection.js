c6f817d139049e9dfe06d291135620fec95a08b2
barakformanski <barakformanski@gmail.com>
2020-12-28 00:41:45 +0200

    fix MinChiburNoDirection

--- grass-calculator/src/minChiburNoDirection.js
+++ grass-calculator/src/minChiburNoDirection.js
@@ -1,13 +1,10 @@
-function MinChiburNoDirection(arrX, arrY) {
-  let returnedArr = [];
 
+function MinChiburNoDirection(arrX, arrY) {
+
+  let returnedArr = [];
   for (let i = 0; i < arrX.length; i++) {
-    let test = arrX[i].opt2;
-    console.log("test : ", test);
-    let optX = arrX[i].opt2.amount + arrX[i].opt3.amount + arrX[i].opt4.amount;
-    console.log(optX);
+    let optX = arrX[i].opt2.amount + arrX[i].opt3.amount + arrX[i].opt4.amount;  
     let optY = arrY[i].opt2.amount + arrY[i].opt3.amount + arrY[i].opt4.amount;
-    console.log(optY);
     if (optX === optY) {
       if (arrX[i].pchat > arrY[i].pchat) {
         returnedArr.push(arrY[i]);
@@ -15,14 +12,14 @@ function MinChiburNoDirection(arrX, arrY) {
         returnedArr.push(arrX[i]);
       }
     }
-
-    if (optX > optY) {
-      returnedArr.push(optY[i]);
-    } else {
-      returnedArr.push(optX[i]);
+    else {
+      if (optX > optY) {
+        returnedArr.push(arrY[i]);
+      } else {
+        returnedArr.push(arrX[i]);
+      }
     }
   }
-
   return returnedArr;
 }
 
